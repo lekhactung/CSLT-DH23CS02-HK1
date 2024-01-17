2fea96e51202fde1185c9a1e76aed2b6d674da8a
lekhactung <tungle07102005@gmail.com>
2024-01-18 02:11:47 +0700

    them thuat toan

--- ucln.cpp
+++ ucln.cpp
@@ -2,20 +2,33 @@
 #include <string>
 using namespace std;
 
-
-float ucln(int a, int b){
+float ucln(int a,int b)
+{
   int m;
-    if( b ==0 ){
-      cout << "ucln = " << a;
-      return a;
-    } else {
-      m=a%b;
-      return ucln(b,m);
-    }
+  if(b==0){
+    return a;
+  } else {
+    m=a%b;
+    return ucln(b,m);
+  }
+}
+
+float ucln2(int a,int b){
+  while(b!=0){
+    int r=a%b;
+    a=b;
+    b=r;
+  }
+  return a;
+}
+
+float bcnn(int a,int b){
+  return a*b/ucln(a,b);
 }
 
 int main(){
-  ucln(125,0);
-
+  cout << ucln(0,20) << endl;
+  cout << bcnn(20,28) << endl;
+  cout << ucln2(0,20);
   return 0;
 }
