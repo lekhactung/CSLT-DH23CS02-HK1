977bbf69b7795e8c2196efccdd6dbb319c571a69
lekhactung <tungle07102005@gmail.com>
2024-01-19 16:25:08 +0700

    upd

---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+long giaithua(long n){
+    int gt=1;
+    for(int i=1;i<=n;i++){
+        gt*=i;
+    }
+    return gt;
+}
+
+long tohop(long n,long k){
+    int c = giaithua(n)/giaithua(k)/giaithua(n-k);
+    return c;
+}
+
+int main(){
+    long n,k;
+    cin >> n;
+    int n1 =n;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<=n1;j++){
+            cout << " ";
+        }
+        for (int j=0;j<=i;j++){
+            cout << tohop(i,j) << " ";
+        }
+        cout << endl;
+        n1--;
+    }
+    return 0;
+}
